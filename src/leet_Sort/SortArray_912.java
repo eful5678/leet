405426522268b49cdf0c04758a61cc5d1631a721
eful5678 <khjkhj1212@hanmail.net>
2021-01-26 15:15:07 +0900

    Finish Problem
    solved problem : Sort Array 912

--- src/leet_Sort/SortArray_912.java
+++ src/leet_Sort/SortArray_912.java
@@ -10,7 +10,7 @@ public class SortArray_912 {
 		sol.sortArray(nums);
 		
 		for(int i = 0; i < nums.length; i++) {
-			//System.out.println(nums[i]);
+			System.out.println(nums[i]);
 		}
 		
 	}
@@ -28,17 +28,16 @@ class Solution {
         int min;
         for(int i = 0; i < nums.length; i++){
             min = 50000;
-            for(int j = 0; j < nums.length; j++){
+            for(int j = i; j < nums.length; j++){
                 if(min > nums[j]){
                     min = nums[j];
                     index = j;
-                    System.out.println(min);
-                }
+                                    }
             }
             temp = nums[i];
             nums[i] = nums[index];
             nums[index] = temp;
-            System.out.println(nums[i]);
+           
         }
         return nums;
     }
